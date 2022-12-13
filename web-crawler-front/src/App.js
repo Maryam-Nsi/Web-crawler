7fe952efc47dff7e0bf006829e5fc41711722dae
negarbarooti <barootinegar@gmail.com>
2022-12-13 13:44:47 +0330

    start react project

---
+++ web-crawler-front/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SignIn from './SignIn';
+import Header from './Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SignIn />
+    </>
+  );
+}
+
+export default App;
