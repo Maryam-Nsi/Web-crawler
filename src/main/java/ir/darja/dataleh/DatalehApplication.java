2e076477ad8b3425465500c00660dc9fb3c69399
Saleh Javanmard <sjavanmardp@gmail.com>
2022-12-16 09:08:17 +0330

    spring init

---
+++ src/main/java/ir/darja/dataleh/DatalehApplication.java
@@ -0,0 +1,13 @@
+package ir.darja.dataleh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatalehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatalehApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/darja/dataleh/DatalehApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.darja.dataleh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatalehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
