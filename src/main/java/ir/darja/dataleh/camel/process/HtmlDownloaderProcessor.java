bb8fce5f47c5c9d50491685d50f2f4a82d792eeb
Saleh Javanmard <sjavanmardp@gmail.com>
2023-01-19 16:10:34 +0330

    use jsoup

---
+++ src/main/java/ir/darja/dataleh/camel/process/HtmlDownloaderProcessor.java
@@ -0,0 +1,24 @@
+package ir.darja.dataleh.camel.process;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HtmlDownloaderProcessor implements Processor {
+    @Value("${jsoup.default-user-agent}")
+    private String userAgent;
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        String url = exchange.getMessage().getBody(String.class);
+        if (url == null) return;
+        Connection jsoup = Jsoup.connect(url);
+        Document document = jsoup.userAgent(userAgent).timeout(15000).get();
+        exchange.getMessage().setBody(document);
+    }
+}
--- src/main/java/ir/darja/dataleh/camel/route/URLInjectionRoute.java
+++ src/main/java/ir/darja/dataleh/camel/route/URLInjectionRoute.java
@@ -1,17 +1,29 @@
 package ir.darja.dataleh.camel.route;
 
+import ir.darja.dataleh.camel.process.HtmlDownloaderProcessor;
+import ir.darja.dataleh.camel.process.URLInjectorProcessor;
+import lombok.RequiredArgsConstructor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class URLInjectionRoute extends RouteBuilder {
     @Value("${URLInjection.consumerSeda}")
     private String consumerSeda;
+    private final URLInjectorProcessor urlInjectorProcessor;
+    private final HtmlDownloaderProcessor htmlDownloaderProcessor;
 
     @Override
     public void configure() throws Exception {
         from(consumerSeda)
-                .process(p -> System.err.println("hihihihih"));
+                .process(urlInjectorProcessor)
+                .to("direct:htmlDownloader");
+
+        from("direct:htmlDownloader")
+                .split().body()
+                .process(htmlDownloaderProcessor);
+
     }
 }
